837171f7673b3045cc220324ee4f123e09bb617c
WATTBA <nugentc@uoguelph.ca>
2017-11-12 13:14:47 -0500

    neural network from scratch, gini issue

---
+++ gini.py
@@ -0,0 +1,13 @@
+def gini(actual, pred, cmpcol = 0, sortcol = 1):
+     assert( len(actual) == len(pred) )
+     all = np.asarray(np.c_[ actual, pred, np.arange(len(actual)) ], dtype=np.float)
+     all = all[ np.lexsort((all[:,2], -1*all[:,1])) ]
+     totalLosses = all[:,0].sum()
+     giniSum = all[:,0].cumsum().sum() / totalLosses
+ 
+     giniSum -= (len(actual) + 1) / 2.
+     return giniSum / len(actual)
+ 
+ def gini_normalized(a, p):
+     return gini(a, p) / gini(a, a)
+ 
