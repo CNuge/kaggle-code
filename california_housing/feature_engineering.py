d673747841541ec9da628a86753e732c96515486
WATTBA <nugentc@uoguelph.ca>
2018-01-08 07:01:43 -0500

    lat long data

---
+++ california_housing/feature_engineering.py
@@ -0,0 +1,12 @@
+
+import pandas as pd 
+import numpy as np
+
+
+"""
+cal_cities_lat_long.csv modified from: http://52.26.186.219/internships/useit/content/cities-california-latitude-and-longitude
+
+cal_populations.csv modified from: http://www.dof.ca.gov/Reports/Demographic_Reports/documents/2010-1850_STCO_IncCities-FINAL.xls
+
+
+"""
