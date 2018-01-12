b7990885d8b26b9404fd9ce952b0b2f005019594
WATTBA <nugentc@uoguelph.ca>
2018-01-12 07:06:24 -0500

    feature engineering cont

--- california_housing/feature_engineering.py
+++ california_housing/feature_engineering.py
@@ -31,59 +31,6 @@ housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
 #look a the categories
 housing["income_cat"].hist()
 
-#make a stratified split of the data
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-	train_set = housing.loc[train_index]
-	test_set = housing.loc[test_index]
-
-for set_ in (train_set, test_set):
-	set_.drop("income_cat", axis=1, inplace=True)
-
-gc.collect()
-
-#####
-# plot data 
-#####
-
-
-train_set.plot(kind='scatter', x='longitude', y='latitude', alpha=0.4,
-				s=train_set['population']/100, label='population', figsize=(10,7),
-				c='median_house_value', cmap=plt.get_cmap('jet'), colorbar=True)
-plt.legend() 
-plt.show()
-
-attributes = ['median_house_value', 'median_income', 'total_rooms', 'housing_median_age']
-
-scatter_matrix(housing[attributes], figsize=(12,8))
-
-
-
-#####
-# Alter existing features
-#####
-
-# total rooms --> rooms_per_household
-# total bedrooms --> bedrooms per household
-
-def housing_data_clean(input_df):
-	input_df['rooms_per_household'] = input_df['total_rooms']/input_df['households']
-	input_df['bedrooms_per_household'] = input_df['total_bedrooms']/input_df['households']
-	input_df['bedrooms_per_room'] = input_df['total_bedrooms']/input_df['total_rooms']
-	input_df['population_per_household'] = input_df['population']/input_df['households']
-	input_df = input_df.drop(['total_bedrooms','total_rooms'], axis=1)
-	return input_df
-
-train_set = housing_data_clean(train_set)
-train_set.head()
-#do the same to the test set at the same time so they remain consistent with one another!
-test_set = housing_data_clean(test_set)
-
-X_train = train_set.drop('median_house_value', axis=1)
-y_train = train_set['median_house_value'].values.astype(float)
-
-X_test = test_set.drop('median_house_value', axis=1)
-y_test = test_set['median_house_value'].values.astype(float)
 
 
 
@@ -119,6 +66,7 @@ len(present)
 len(absent)
 absent
 """
+
 city_coords = {}
 
 for dat in city_lat_long.iterrows():
@@ -191,6 +139,88 @@ for key, value in city_coords.items():
 #then do df.apply(closest_point(point, to closest big city))
 
 
+#######
+# adding closest city data to dataframes
+#######
+
+housing['close_city'] = housing.apply(lambda x: 
+							closest_point((x['latitude'],x['longitude']),city_coords), axis = 1)
+housing['close_city_name'] = [x[0] for x in housing['close_city'].values]
+housing['close_city_dist'] = [x[1] for x in housing['close_city'].values]
+
+housing = housing.drop('close_city', axis=1)
+housing.head()
+
+
+#add the data relating to the points to the closest big city
+housing['big_city'] = housing.apply(lambda x: 
+							closest_point((x['latitude'],x['longitude']),big_cities), axis = 1)
+housing['big_city_name'] = [x[0] for x in housing['big_city'].values]
+housing['big_city_dist'] = [x[1] for x in housing['big_city'].values]
+
+housing = housing.drop('big_city', axis=1)
+
+
+#I moved the feature engineering before the train/test split so that we can get
+#and even stratified shuffle split of the closest cities into the two sets
+
+
+#make a stratified split of the data
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+	train_set = housing.loc[train_index]
+	test_set = housing.loc[test_index]
+
+for set_ in (train_set, test_set):
+	set_.drop("income_cat", axis=1, inplace=True)
+
+gc.collect()
+
+#####
+# plot data 
+#####
+
+
+train_set.plot(kind='scatter', x='longitude', y='latitude', alpha=0.4,
+				s=train_set['population']/100, label='population', figsize=(10,7),
+				c='median_house_value', cmap=plt.get_cmap('jet'), colorbar=True)
+plt.legend() 
+plt.show()
+
+attributes = ['median_house_value', 'median_income', 'total_rooms', 'housing_median_age']
+
+scatter_matrix(housing[attributes], figsize=(12,8))
+
+
+
+#####
+# Alter existing features
+#####
+
+# total rooms --> rooms_per_household
+# total bedrooms --> bedrooms per household
+
+def housing_data_clean(input_df):
+	input_df['rooms_per_household'] = input_df['total_rooms']/input_df['households']
+	input_df['bedrooms_per_household'] = input_df['total_bedrooms']/input_df['households']
+	input_df['bedrooms_per_room'] = input_df['total_bedrooms']/input_df['total_rooms']
+	input_df['population_per_household'] = input_df['population']/input_df['households']
+	input_df = input_df.drop(['total_bedrooms','total_rooms'], axis=1)
+	return input_df
+
+train_set = housing_data_clean(train_set)
+train_set.head()
+#do the same to the test set at the same time so they remain consistent with one another!
+test_set = housing_data_clean(test_set)
+
+X_train = train_set.drop('median_house_value', axis=1)
+y_train = train_set['median_house_value'].values.astype(float)
+
+X_test = test_set.drop('median_house_value', axis=1)
+y_test = test_set['median_house_value'].values.astype(float)
+
+
+
 
 #####
 # fill numerical values
@@ -235,9 +265,13 @@ for i in missing_vals:
 ####
 encoder = LabelBinarizer()
 
-encoded_ocean_train_1hot = encoder.fit_transform(X_train['ocean_proximity'])
+encoded_ocean_train_1hot = encoder.fit_transform(X_train[['ocean_proximity', 
+															'close_city_name',
+															'big_city_name']])
 #I'm using just transform below to ensure that the categories are sorted and used the same as in the train fit.
-encoded_ocean_test_1hot = encoder.transform(X_test['ocean_proximity'])
+encoded_ocean_test_1hot = encoder.transform(X_test[['ocean_proximity', 
+													'close_city_name',
+													'big_city_name']])
 
 
 train_cat_df = pd.DataFrame(encoded_ocean_train_1hot, index = X_train.index, columns = encoder.classes_ )
