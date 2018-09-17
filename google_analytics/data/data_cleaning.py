7c3e2ec1c2fdc1b768dc9531fbec51bc5ef5f714
Cam Nugent <Cam@Minerva.local>
2018-09-17 07:11:28 -0400

    parsing the json to clean df

---
+++ google_analytics/data/data_cleaning.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from pandas import Series, DataFrame
+import numpy as np
+
+import json
+
+
+test = pd.read_csv('test.csv')
+
+test.head()
+
+#which columns have json
+#device
+json_cols = ['device', 'geoNetwork', 'totals',  'trafficSource']
+column = 'device'
+
+for column in json_cols:
+
+c_load = test[column].apply(json.loads)
+c_list = list(c_load)
+c_dat = json.dumps(c_list)
+
+test = test.join(pd.read_json(c_dat))
+test.drop(column , axis=1)
+
+test.head()
+test.to_csv('test_cleaned.csv')
+
+
+train = pd.read_csv('train.csv')
+train.head()
+
+#which columns have json
+#device
+json_cols = ['device', 'geoNetwork', 'totals',  'trafficSource']
+column = 'device'
+
+for column in json_cols:
+
+c_load = train[column].apply(json.loads)
+c_list = list(c_load)
+c_dat = json.dumps(c_list)
+
+train = train.join(pd.read_json(c_dat))
+train.drop(column , axis=1)
+
+train.head()
+train.to_csv('test_cleaned.csv')
