6bd7fc12a7e94dd18d5ce3eee200a6083ccf8756
CNuge <nugentc@uoguelph.ca>
2018-10-18 11:03:08 -0400

    working... poorly

--- google_analytics/light_gbm_predict.py
+++ google_analytics/light_gbm_predict.py
@@ -10,19 +10,19 @@ y_train.shape
 X_test.shape 
 
 #split off a validation set
-X_trainr, X_val, y_trainr, y_val = train_test_split(X, y, test_size=0.2, random_state=1738)
+X_trainr, X_val, y_trainr, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1738)
+
 
 #basic starting params, can tune via grid search once things moving well
 lgb_params = {
 	"seed": 1738,
+	"bagging_seed" : 42,
 	"objective" : "regression",
 	"metric" : "rmse",
 	"num_leaves" : 40,
-	"learning_rate" : 0.01,
+	"learning_rate" : 0.001,
 	"bagging_fraction" : 0.5,
 	"feature_fraction" : 0.5,
-	"bagging_frequency" : 6,
-	"bagging_seed" : 42,
 	}
 
 #load in lgbm matrix fmt
@@ -30,10 +30,10 @@ ltrain = lgb.Dataset(X_trainr, label=y_trainr)
 lval = lgb.Dataset(X_val, label=y_val)
 
 #build and train the model
-lgb_model1 = lgb.train(params, lgb_train_data, 
-                  num_boost_round=2000,
-                  valid_sets=[lgb_train_data, lgb_val_data],
-                  early_stopping_rounds=50,
+lgb_model1 = lgb.train(lgb_params, ltrain, 
+                  num_boost_round=5000,
+                  valid_sets=[ltrain, lval],
+                  early_stopping_rounds=100,
                   verbose_eval=100)
 
 
@@ -44,21 +44,41 @@ test_y = lgb_model1.predict(X_test, num_iteration = lgb_model1.best_iteration)
 # aggregate on 'fullVisitorId' 
 # final_test['fullVisitorId' ]
 
-final_pred = final_test['fullVisitorId']
+final_pred = final_test[['fullVisitorId']].copy()
 
-final_pred[train_yht] = test_y
+final_pred['train_yht'] = test_y
+
+def set_floor(x):
+	if x < 0:
+		return 0
+	else:
+		return x
 
 
-final_pred = final_pred.sort(['fullVisitorId'])
+#issue - the ids in the submission file and the ids in the test aren't a 1:1 match?
+#have I jumbled them or something?
+
+
+final_pred['train_yht'] = final_pred['train_yht'].apply(lambda x: set_floor(x))
+
 
 final_by_ind =  final_pred.groupby(['fullVisitorId']).sum()
 
-final_by_ind = final_by_ind.add_suffix('_sum').reset_index()
+final_by_ind = final_by_ind.reset_index()
 
-final_by_ind['PredictedLogRevenue'] = np.log1p(final_by_ind['train_yht_sum'])
+final_by_ind.head()
+
+submission = submission.merge(final_by_ind, on='fullVisitorId', how='left')
+
+submission['PredictedLogRevenue'] = np.log1p(submission['train_yht'])
+
+submission['PredictedLogRevenue'] = submission['PredictedLogRevenue'].fillna(0)
+
+submission = submission.drop(['train_yht'], axis=1)
+submission.head()
 
 #submit
-final_by_ind.to_csv('cam_pred1.csv')
+submission.to_csv('cam_pred1.csv', index=False)
 
 """
 #once the above is working try the following:
