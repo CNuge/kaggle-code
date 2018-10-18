e6c2d92611873b5793bde14e648ffd1c15deba11
CNuge <nugentc@uoguelph.ca>
2018-10-18 14:37:07 -0400

    working on the model

--- google_analytics/light_gbm_predict.py
+++ google_analytics/light_gbm_predict.py
@@ -10,7 +10,7 @@ y_train.shape
 X_test.shape 
 
 #split off a validation set
-X_trainr, X_val, y_trainr, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1738)
+X_trainr, X_val, y_trainr, y_val = train_test_split(X_train, y_train, test_size = 0.2, random_state = 1738)
 
 
 #basic starting params, can tune via grid search once things moving well
@@ -26,15 +26,15 @@ lgb_params = {
 	}
 
 #load in lgbm matrix fmt
-ltrain = lgb.Dataset(X_trainr, label=y_trainr)
-lval = lgb.Dataset(X_val, label=y_val)
+ltrain = lgb.Dataset(X_trainr, label = y_trainr)
+lval = lgb.Dataset(X_val, label = y_val)
 
 #build and train the model
 lgb_model1 = lgb.train(lgb_params, ltrain, 
-                  num_boost_round=50000,
-                  valid_sets=[ltrain, lval],
-                  early_stopping_rounds=500,
-                  verbose_eval=100)
+                  num_boost_round = 50000,
+                  valid_sets = [ltrain, lval],
+                  early_stopping_rounds = 500,
+                  verbose_eval = 100)
 
 
 #make predictions on the test data
@@ -49,6 +49,7 @@ final_pred = final_test[['fullVisitorId']].copy()
 
 final_pred['train_yht'] = test_y
 
+"""
 ########
 # experiment - try without this as well see if setting the floor is causing the issue
 def set_floor(x):
@@ -59,7 +60,7 @@ def set_floor(x):
 
 final_pred['train_yht'] = final_pred['train_yht'].apply(lambda x: set_floor(x))
 ####
-
+"""
 
 #issue - the ids in the submission file and the ids in the test aren't a 1:1 match?
 #have I jumbled them or something?
@@ -71,17 +72,17 @@ final_by_ind = final_by_ind.reset_index()
 
 final_by_ind.head()
 
-submission = submission.merge(final_by_ind, on='fullVisitorId', how='left')
+submission = submission.merge(final_by_ind, on = 'fullVisitorId', how = 'left')
 
 submission['PredictedLogRevenue'] = np.log1p(submission['train_yht'])
 
 submission['PredictedLogRevenue'] = submission['PredictedLogRevenue'].fillna(0)
 
-submission = submission.drop(['train_yht'], axis=1)
+submission = submission.drop(['train_yht'], axis = 1)
 submission.head()
 
 #submit
-submission.to_csv('cam_pred1.csv', index=False)
+submission.to_csv('cam_pred1.csv', index = False)
 
 """
 #once the above is working try the following:
