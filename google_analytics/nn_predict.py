49ad83f45d975f7f7aad9216dcea21bb24063aca
CNuge <nugentc@uoguelph.ca>
2018-10-19 12:43:46 -0400

    Model diverged with loss = NaN. suspect this is too many 0s

--- google_analytics/nn_predict.py
+++ google_analytics/nn_predict.py
@@ -14,20 +14,27 @@ y_train = np.float32(y_train)
 X_test = np.float32(X_test)
 
 
+"""
+I suspect this may not work the first time around.... can try to downsample the 0s
+in order to give a more balanced dataset.... possibly 3:1 0s:purchases
+"""
+
+
+
+####
+# dnn
+####
+
+
+feature_cols = [tf.feature_column.numeric_column("X", shape=[X_train.shape[1]])]
 
 dnn_clf = tf.estimator.DNNRegressor(hidden_units=[150,300,900,300,150],
-                                         feature_columns=feature_cols, config=config)
-
-dnn_clf.train(X_train, y_train, steps=400)
-
-
-feature_cols = [tf.feature_column.numeric_column("X", shape=[91])]
-dnn_clf = tf.estimator.DNNRegressor(hidden_units=[300,100],
                                      feature_columns=feature_cols)
 
 input_fn = tf.estimator.inputs.numpy_input_fn(
     x={"X": X_train}, y=y_train, num_epochs=40, batch_size=50, shuffle=True)
 
+dnn_clf.train(input_fn=input_fn)
 
 #the output is a generator object
 dnn_y_pred = dnn_clf.predict(X_test)
@@ -59,9 +66,4 @@ submission['PredictedLogRevenue'] = submission['test_pred'].fillna(0.0)
 submission = submission.drop(['test_pred'], axis = 1)
 
 #submit the output
-submission.to_csv('cam_dnn_pred2.csv', index = False)
-
-"""
-I suspect this may not work the first time around.... can try to downsample the 0s
-in order to give a more balanced dataset.... possibly 3:1 0s:purchases
-"""
+submission.to_csv('cam_dnn_pred1.csv', index = False)
